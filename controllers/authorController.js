7c925f3ab43b8ccb7447d3b15adb07262b2a675b
shady <shadyngheith@gmail.com>
2024-05-17 10:31:47 +0300

    Develop author-create-get

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,8 @@
 const Author = require("../models/author");
 const Book = require("../models/book");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
+
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -35,13 +37,60 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 
 // Display Author create form on GET.
 exports.author_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create GET");
+  res.render("author_form", { title: "Create Author" });
 });
 
 // Handle Author create on POST.
-exports.author_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author create POST");
-});
+exports.author_create_post = [
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" }) // means that we'll accept either an empty string or null as an empty value)
+    .isISO8601()
+    .toDate(), // to cast these to the proper JavaScript types
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+    if (!errors.isEmpty()) {
+      res.render('auther_form', {
+        title: 'Author form',
+        author: author,
+        erroer: errors.array()
+      })
+    }
+    else {
+      const autherExist = await Author.findOne({
+        first_name: author.first_name,
+        family_name: author.family_name
+      }).collation({ locale: 'en', strength: 2 })
+        .exec();
+      if (autherExist) {
+        res.redirect(autherExist.url)
+      }
+      else {
+        await author.save()
+        res.redirect(author.url);
+      }
+    }
+  })
+];
 
 // Display Author delete form on GET.
 exports.author_delete_get = asyncHandler(async (req, res, next) => {
