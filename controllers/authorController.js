fe22b583e856037a94c8af5b5a5c442d3f42d2a0
shady <shadyngheith@gmail.com>
2024-05-19 13:44:41 +0300

    Develop author update route

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -132,10 +132,52 @@ exports.author_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display Author update form on GET.
 exports.author_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update GET");
+  const author = await Author.findById(req.params.id);
+  res.render('author_form', {
+    title: 'Updata Author',
+    author: author
+  });
 });
 
 // Handle Author update on POST.
-exports.author_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Author update POST");
-});
+exports.author_update_post = [
+  body("first_name", 'First name must be specified.')
+    .trim()
+    .isLength({ min: 1})
+    .escape(),
+  body("family_name", 'Family name must be specified.')
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ values: "falsy" }) // means that we'll accept either an empty string or null as an empty value)
+    .isISO8601()
+    .toDate(), // to cast these to the proper JavaScript types
+  body("date_of_death", "Invalid date of death")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+  asyncHandler(async (req, res, next) => {
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+      _id: req.params.id
+    });
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'Updata Author',
+        author: author,
+        errors: errors.array()
+      });
+    }
+    else {
+      const updatedAuthor = await Author.findByIdAndUpdate(req.params.id, author, {});
+      res.redirect(updatedAuthor.url);
+    }
+  })
+]
